f6e19728fd439f8bc6f9a52917920d29314479cb
Ian <iss13@my.fsu.edu>
2015-09-27 16:53:44 -0400

    Update shell.c

--- Source Code/shell.c
+++ Source Code/shell.c
@@ -11,14 +11,6 @@
 
 /*Included libraries*/
 #include "shell_util.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <stdbool.h>
 
 int main(){
 	char *line;
